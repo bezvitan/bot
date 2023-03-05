41d16e41935b90dce8ed2ead6937c9473a298e02
Vitalii Bezgin <64967238+bezvitan@users.noreply.github.com>
2023-03-05 22:09:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,47 +1,21 @@
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-TOKEN = os.getenv('TOKEN')
-
-updater = Updater(TOKEN, use_context=True)
-
-
-# обработка команды /start
 def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Привет! Я бот, который может подсказать погоду и новости. Напишите /help, чтобы узнать все мои команды.")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, I'm a bot!")
 
-# обработка команды /help
-def help(update, context):
-    help_text = "Я могу выполнить следующие команды:\n\n"
-    help_text += "/start - запуск бота\n"
-    help_text += "/help - вывод всех команд\n"
-    help_text += "/weather <город> - погода в указанном городе\n"
-    help_text += "/news - последние новости\n"
-    context.bot.send_message(chat_id=update.message.chat_id, text=help_text)
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
-# обработка команды /weather
-def weather(update, context):
-    city = context.args[0]
-    weather_api_key = os.environ['WEATHER_API_KEY']
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}'
-    response = requests.get(url)
-    weather_data = response.json()
-    temperature = weather_data['main']['temp']
-    humidity = weather_data['main']['humidity']
-    weather_text = f'Погода в городе {city}: температура {temperature}°C, влажность {humidity}%'
-    context.bot.send_message(chat_id=update.message.chat_id, text=weather_text)
+def main():
+    updater = Updater('5927896952:AAHFiVk7mJnuc4bNz7mVEt_GCthFmvYbrxE', use_context=True)
 
-# обработка команды /news
-def news(update, context):
-    news_api_key = os.environ['NEWS_API_KEY']
-    url = f'https://newsapi.org/v2/top-headlines?country=ru&apiKey={news_api_key}'
-    response = requests.get(url)
-    news_data = response.json()
-    articles = news_data['articles']
-    news_text = ''
-    for article in articles:
-        news_text += f"\n{article['title']}\n{article['url']}\n"
-    context.bot.send
-git push origin main
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
